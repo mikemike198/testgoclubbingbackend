4a138d1cdafb483430c081122efe844da9216358
Miguel Garcia Marques <61141327+mikemike198@users.noreply.github.com>
2021-06-28 15:00:13 +0100

    additional files and changes for Heroku

--- goclubbing/goclubbing/settings.py
+++ goclubbing/goclubbing/settings.py
@@ -85,12 +85,7 @@ WSGI_APPLICATION = 'goclubbing.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+
 
 DATABASES = {
     'default': {
