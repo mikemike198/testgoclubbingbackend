15e2dc88ae0798f54c6bc6618eda0e67a5d9a2b0
Miguel Garcia Marques <61141327+mikemike198@users.noreply.github.com>
2021-06-28 16:04:43 +0100

    additional files and changes

--- goclubbing/goclubbing/settings.py
+++ goclubbing/goclubbing/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = '0$o*(0nnpi((6-!)oczcro-nd_!b)*(2u-$qx$szks2^jw6p3q'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
